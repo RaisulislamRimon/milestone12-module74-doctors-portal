b3ec24ee78d6c529ad45306880f1704d43269cf0
RaisulislamRimon <rimonppip0@gmail.com>
2022-11-18 12:04:47 +0600

    React Query and load data

--- doctors-portal-client/src/Pages/Appointment/AvailableAppointments/AvailableAppointments.js
+++ doctors-portal-client/src/Pages/Appointment/AvailableAppointments/AvailableAppointments.js
@@ -1,20 +1,36 @@
+import { useQuery } from "@tanstack/react-query";
 import { format } from "date-fns";
 import React from "react";
 import AppointmentOption from "../AppointmentOption/AppointmentOption";
 import BookingModal from "../BookingModal/BookingModal";
 
 const AvailableAppointments = ({ selectedDate, setSelectedDate }) => {
-  const [appointmentOptions, setAppointmentOptions] = React.useState([]);
+  // const [appointmentOptions, setAppointmentOptions] = React.useState([]);
   const [treatment, setTreatment] = React.useState(null);
 
-  React.useEffect(() => {
-    fetch("https://doctors-portal-server-sigma.vercel.app/appointmentOptions")
-    // fetch("http://localhost:5000/appointmentOptions")
-      .then((response) => response.json())
-      .then((data) => {
-        setAppointmentOptions(data);
-      });
-  }, [selectedDate]);
+  const { data: appointmentOptions = [] } = useQuery({
+    queryKey: [`appointmentOptions`],
+    queryFn: async () => {
+      const response = await fetch(
+        `https://doctors-portal-server-sigma.vercel.app/appointmentOptions`
+      );
+      const data = await response.json();
+      return data;
+    },
+    // queryFn: () =>
+    //   fetch(
+    //     `https://doctors-portal-server-sigma.vercel.app/appointmentOptions`
+    //   ).then((response) => response.json()),
+  });
+
+  // React.useEffect(() => {
+  //   fetch("https://doctors-portal-server-sigma.vercel.app/appointmentOptions")
+  //   // fetch("http://localhost:5000/appointmentOptions")
+  //     .then((response) => response.json())
+  //     .then((data) => {
+  //       setAppointmentOptions(data);
+  //     });
+  // }, [selectedDate]);
   return (
     <section className="my-16">
       <p className="text-center text-emerald-500 font-bold">
